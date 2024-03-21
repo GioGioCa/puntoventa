31320c26e8aaa052250ff731d4680403c0400cc6
GioGioCa <darkconor2500@hotmail.com>
2024-03-21 02:41:52 -0600

    Feat(App): Creada ventana principal Punto Venta

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, FlatList} from 'react-native';
+import styles from './Styles';
+import RenderItem from './components/renderItem';
+import {Header} from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return (
+    // Toda la ventana
+    <View style={styles.container}>
+      <Header centerComponent={{text: 'Ferreteria', style: styles.title}} />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnInventory}>
+          <Text style={styles.btnTxt}>Inventario</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btnStore}>
+          <Text style={styles.btnTxt}>Tienda</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
---
+++ Styles.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet, Dimensions} from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 25,
+        color: '#273746',
+    },
+    btnContainer: {
+        flexDirection: 'column',
+        marginVertical: 20,
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        borderRadius: 30,
+        width: Dimensions.get('screen').width * 0.25,
+        height: Dimensions.get('screen').width * 0.80,
+    },
+    btnInventory: {
+        backgroundColor: '#5DADE2',
+        height: '30%',
+        width: '90%',
+    },
+    btnStore: {
+        backgroundColor: '#CD6155',
+        height: '30%',
+        width: '90%',
+    },
+    btnTxt: {
+        fontSize: 20,
+        color: '#F7F9F9',
+    },
+});
+
+export default styles;
